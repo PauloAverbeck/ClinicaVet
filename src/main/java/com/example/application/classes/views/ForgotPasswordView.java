71cf23bfac9e7ad1738ed4c31eaa6c8fa99af12b
Paulo H. Averbeck <69552119+pHaverbeck@users.noreply.github.com>
2025-10-17 17:19:22 -0300

    Adicionado envio de email para redefinir senha via forgot password
    Corrigido posicionamento de campo email na ForgotPasswordView

--- src/main/java/com/example/application/classes/views/ForgotPasswordView.java
+++ src/main/java/com/example/application/classes/views/ForgotPasswordView.java
@@ -2,14 +2,12 @@ package com.example.application.classes.views;
 
 import com.example.application.classes.AppUserService;
 import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.router.PageTitle;
@@ -35,14 +33,14 @@ public class ForgotPasswordView extends VerticalLayout {
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
+        setAlignItems(Alignment.CENTER);
         setSpacing(true);
 
         final var title = new H1("Esqueci minha senha");
         final var subtitle = new Paragraph("Informe o seu e-mail. Se houver uma conta associada, você receberá um e-mail com instruções para redefinir sua senha.");
 
         email.setClearButtonVisible(true);
-        email.setWidth("400px");
+        email.setWidth("300px");
         email.setErrorMessage("Informe um e-mail válido");
         email.setRequiredIndicatorVisible(true);
 
@@ -56,23 +54,20 @@ public class ForgotPasswordView extends VerticalLayout {
         enviarBtn.addClickShortcut(Key.ENTER);
         enviarBtn.addClickListener(e -> onSubmit());
         enviarBtn.getElement().setProperty("title", "Enviar link de redefinição de senha");
+        enviarBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 
         voltarLoginBtn.addClickListener(e -> getUI().ifPresent(ui -> ui.navigate("login")));
         voltarLoginBtn.getElement().setProperty("title", "Voltar para a tela de login");
+        voltarLoginBtn.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
 
-        final var form = new FormLayout();
-        form.setMaxWidth("480px");
-        form.add(email);
-        form.setResponsiveSteps(
-                new FormLayout.ResponsiveStep("0", 1)
-        );
+        VerticalLayout form = new VerticalLayout(email, enviarBtn, voltarLoginBtn);
+        form.add(email, enviarBtn, voltarLoginBtn);
+        form.setWidth("400px");
+        form.setAlignItems(Alignment.CENTER);
+        form.setPadding(false);
+        form.setSpacing(true);
 
-        final var actions = new VerticalLayout(enviarBtn, voltarLoginBtn);
-        actions.setPadding(false);
-        actions.setSpacing(true);
-        actions.setWidth(400, Unit.PIXELS);
-
-        add(title, subtitle, form, actions);
+        add(title, subtitle, form);
     }
 
     private void onSubmit() {

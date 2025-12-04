4968a6ae7cd8d1486745fc875c46df8cd94fc74f
Paulo H. Averbeck <69552119+pHaverbeck@users.noreply.github.com>
2025-12-04 17:32:14 -0300

    - Corrigido Migration.

--- src/main/resources/db/migration/V15__add_created_by_user.sql
+++ src/main/resources/db/migration/V15__add_created_by_user.sql
@@ -1,9 +1,9 @@
 ALTER TABLE client
-    ADD COLUMN created_by_user_id BIGINT,
+    ADD COLUMN IF NOT EXISTS created_by_user_id BIGINT,
     ADD CONSTRAINT fk_client_created_by_user
         FOREIGN KEY (created_by_user_id) REFERENCES app_user(id);
 
 ALTER TABLE pet
-    ADD COLUMN created_by_user_id BIGINT,
+    ADD COLUMN IF NOT EXISTS created_by_user_id BIGINT,
     ADD CONSTRAINT fk_pet_created_by_user
         FOREIGN KEY (created_by_user_id) REFERENCES app_user(id);
